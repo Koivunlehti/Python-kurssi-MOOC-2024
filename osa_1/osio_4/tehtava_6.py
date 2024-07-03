568f8acf7a90d04186695019913ef212c32fa4a6
Koivunlehti <56386375+Koivunlehti@users.noreply.github.com>
2024-07-03 16:52:12 +0300

    osa 1.4 tehtava 6

---
+++ osa_1/osio_4/tehtava_6.py
@@ -0,0 +1,8 @@
+number_1 = int(input("Luku 1: "))
+number_2 = int(input("Luku 2: "))
+number_3 = int(input("Luku 3: "))
+number_4 = int(input("Luku 4: "))
+
+sum = number_1 + number_2 + number_3 + number_4
+
+print(f"Lukujen summa on {sum} ja keskiarvo {sum / 4}")
