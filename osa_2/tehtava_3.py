3aa7ae85730f547c71e0e0c21badff0a7b993010
Koivunlehti <56386375+Koivunlehti@users.noreply.github.com>
2024-07-03 15:23:30 +0300

    osa 1, osio 2, tehtava 3

---
+++ osa_2/tehtava_3.py
@@ -0,0 +1,7 @@
+name = input("Etunimi: ")
+lastname = input("Sukunimi: ")
+address = input("Katuosoite: ")
+zip_and_city = input("Postinumero ja kaupunki: ")
+print(name + " " + lastname)
+print(address)
+print(zip_and_city)
