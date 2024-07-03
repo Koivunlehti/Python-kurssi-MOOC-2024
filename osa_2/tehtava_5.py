08288a71c6aa2e50a9c0844c967772fec8bc8e7f
Koivunlehti <56386375+Koivunlehti@users.noreply.github.com>
2024-07-03 15:32:45 +0300

    osa 1, osio 2, tehtava 5

---
+++ osa_2/tehtava_5.py
@@ -0,0 +1,6 @@
+name = input("Anna nimi: ")
+year = input("Anna vuosi: ")
+
+print(name + " on urhea ritari, syntynyt vuonna " + year + 
+      ". Eräänä aamuna " + name + " heräsi kovaan meluun: lohikäärme lähestyi kylää. Vain " + name + 
+      " voisi pelastaa kylän asukkaat.")
