084b6b81a9c9e6602ce988873dc1f839511b2489
Koivunlehti <56386375+Koivunlehti@users.noreply.github.com>
2024-07-04 12:51:47 +0300

    osa 1.5 tehtävät 2 ja 3

---
+++ osa_1/osio_5/tehtava_2.py
@@ -0,0 +1,6 @@
+number = int(input("Anna luku: "))
+
+if number < 0:
+    number *= -1
+
+print(f"Luvun itseisarvo on {number}")
---
+++ osa_1/osio_5/tehtava_3.py
@@ -0,0 +1,7 @@
+name = input
+
+if input("Mikä on nimesi? ") != "Jerry":
+    amount = int(input("Kuinka monta annosta keittoa: "))
+    print(f"Kokonaishinta on {amount * 5.90}")
+
+print("Seuraava!")
