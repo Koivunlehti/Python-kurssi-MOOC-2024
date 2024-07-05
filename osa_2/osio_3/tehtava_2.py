2ae22240bf82da5d8f87daf7af09e1ac1753a573
Koivunlehti <56386375+Koivunlehti@users.noreply.github.com>
2024-07-05 17:20:16 +0300

    osa 2.3 tehtävä 2

---
+++ osa_2/osio_3/tehtava_2.py
@@ -0,0 +1,8 @@
+name = input("Anna nimesi: ")
+
+if name == "Tupu" or name == "Hupu" or name == "Lupu":
+    print("Olet luultavasti Aku Ankan veljenpoika.")
+elif name == "Mortti" or name == "Vertti":
+    print("Olet luultavasti Mikki Hiiren veljenpoika.")
+else:
+    print("Et ole kenenkään tuntemani hahmon veljenpoika.")
