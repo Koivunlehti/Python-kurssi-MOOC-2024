3078f67317d815211000304cc7ded5183c7f7ab9
Koivunlehti <56386375+Koivunlehti@users.noreply.github.com>
2024-07-09 15:12:51 +0300

    osa 3.2 tehtävä 7

---
+++ osa_3/osio_2/tehtava_7.py
@@ -0,0 +1,9 @@
+while True:
+    sentence = input("Anna merkkijono: ")
+    if len(sentence) == 0:
+        break
+    else:
+        print()
+        print(sentence)
+        print("-" * len(sentence))
+        print()
