b38da3b5ff58e7d16d5f72342511081fd7569d84
Koivunlehti <56386375+Koivunlehti@users.noreply.github.com>
2024-07-09 15:02:35 +0300

    osa 3.2 tehtävä 4

---
+++ osa_3/osio_2/tehtava_4.py
@@ -0,0 +1,6 @@
+word = input("Anna sana: ")
+
+if word[1] == word[-2]:
+    print("Toinen ja toiseksi viimeinen kirjain on", word[1])
+else:
+    print("Toinen ja toiseksi viimeinen kirjain eroavat")
