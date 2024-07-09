0f97713bd1fcbcfe7edc2a5b57f58cb94e4e77f3
Koivunlehti <56386375+Koivunlehti@users.noreply.github.com>
2024-07-09 15:49:08 +0300

    osa 3.2 tehtävä 12

---
+++ osa_3/osio_2/tehtava_12.py
@@ -0,0 +1,14 @@
+word = input("Anna merkkijono: ")
+
+if "a" in word:
+    print("a löytyy")
+else:
+    print("a ei löydy")
+if "e" in word:
+    print("e löytyy")
+else:
+    print("e ei löydy")
+if "o" in word:
+    print("o löytyy" )
+else:
+    print("o ei löydy")
