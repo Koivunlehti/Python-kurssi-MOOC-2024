7c0809ae0cbdee26572f2b28cd9f88f4ed5f1b3c
Koivunlehti <56386375+Koivunlehti@users.noreply.github.com>
2024-07-10 12:47:51 +0300

    osa 3.4 tehtävä 1

---
+++ osa_3/osio_4/tehtava_1.py
@@ -0,0 +1,11 @@
+def tulosta_veljekset():
+    print("Aapo")
+    print("Eero")
+    print("Juhani")
+    print("Lauri")
+    print("Simeoni")
+    print("Timo")
+    print("Tuomas")
+    
+if __name__ == "__main__":
+    tulosta_veljekset()
