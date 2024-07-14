eda20d2f63d0127180ba693c52fec57144ef52f2
Koivunlehti <56386375+Koivunlehti@users.noreply.github.com>
2024-07-14 12:59:22 +0300

    osa 4.4 tehtävä 7

---
+++ osa_4/osio_4/tehtava_7.py
@@ -0,0 +1,12 @@
+def parilliset(lista):
+    parilliset_lista = []
+    for luku in lista:
+        if luku % 2 == 0:
+            parilliset_lista.append(luku)
+    return parilliset_lista
+
+if __name__ == "__main__":
+    lista = [1, 2, 3, 4, 5]
+    uusi_lista = parilliset(lista)
+    print("alkuperäinen", lista)
+    print("uusi", uusi_lista)
